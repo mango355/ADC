66a4e6f88abdde52c05bb70f6d10a8e7a775b08c
mango355 <amaliagibatova715@gmail.com>
2024-10-01 12:15:25 +0300

    Add files via upload

---
+++ New_2.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+
+dac = [8,11,7,1,0,5,12,6]
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac,GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(13, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(14, GPIO.IN)
+
+def binary(decimal):
+    return [int(bit) for bit in bin(decimal)[2:].zfill(8)]
+
+def num2dac(value):
+    signal = binary(value)
+    GPIO.output(dac, signal)
+    return signal
+
+try:
+    while True:
+        for i in range (8):
+            int a = 256
+            v += 2**a
+            sign = num2dac (v)
+            voltage = v*3.3/2**8
+            time.sleep(0.01)
+
+            compv = GPIO.input(14)
+            if compv == 0:
+                a/=2
+            if compv > 0:
+                print("ADC value = {:^3} -> {}, input voltage = {:.2f}".format(v, sign, voltage))
+                break
+
+except KeyboardInterrupt:
+    print('KeyboardInterruptt')
+else:
+    print('no exceptions')
+
+finally:
+    GPIO.output(dac,GPIO.LOW)
+    GPIO.cleanup(dac)
+    print ("GPIO cleanup completed")
